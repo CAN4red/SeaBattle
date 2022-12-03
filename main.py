370d71fd60bb038fb805f1748d37b2d843c3cf7a
Alexander Isupov <CAN4red@yandex.ru>
2022-12-03 21:52:37 +0500

    добавил функцию не позваляющую поставить корабль на занятую клетку

--- main.py
+++ main.py
@@ -64,8 +64,50 @@ class SeaBattle():
                 self.player_board[int(point[1]) - 1][6] = '▢'
             elif point[0] == 'H':
                 self.player_board[int(point[1]) - 1][7] = '▢'
+
+        def can_put_point(point): #функция отвечающая за перевод координат в индексы матрицы и постановку в соотвествующую позицию символ '▢'
+            if point[0] == 'A':
+                if self.player_board[int(point[1])-1][0] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'B':
+                if self.player_board[int(point[1]) - 1][1] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'C':
+                if self.player_board[int(point[1]) - 1][2] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'D':
+                if self.player_board[int(point[1]) - 1][3] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'E':
+                if self.player_board[int(point[1]) - 1][4] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'F':
+                if self.player_board[int(point[1]) - 1][5] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'G':
+                if self.player_board[int(point[1]) - 1][6] == '▢':
+                    return True
+                else:
+                    return False
+            elif point[0] == 'H':
+                if self.player_board[int(point[1]) - 1][7] == '▢':
+                    return True
+                else:
+                    return False
         def chekc_of_codinates(point): #функция проверяющая соответствие введенных значений реальным координатам
-            if (point[0] not in string.ascii_uppercase[:8]) or (point[1] not in [str(i) for i in range(1, 9)]):
+            if (point[0] not in string.ascii_uppercase[:8]) or (point[1] not in [str(i) for i in range(1, 9)]) or can_put_point(point):
                 return True
             else: return False
 
@@ -78,7 +120,7 @@ class SeaBattle():
         for i in range(3): #расстановка эсминцев
             a, b = input(f'Эсминцев осталось {3-i}, куда ставим: ').split(' ')
             print(a[0], b[0], a[1], b[1])
-            while chekc_of_codinates(a) and chekc_of_codinates(b):
+            while chekc_of_codinates(a) or chekc_of_codinates(b):
                 a, b = input(f'Oй, косячим! Эсминцев осталось {3 - i}, куда ставим: ').split(' ')
             #print(abs(int(a[1]) - int(b[1])))
             while not xor((a[0] != b[0]) or (abs(int(a[1]) - int(b[1])) != 1), (a[1] != b[1] or ((a[0] + b[0] not in string.ascii_uppercase) and (b[0] + a[0] not in string.ascii_uppercase)))):
